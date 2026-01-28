38699d3f3f5d470b20baae3f2eecda9ecdf3d17a
battulga-ss <blessyou100x@gmail.com>
2026-01-28 15:14:57 +0000

    hi name

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,29 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
+function Welcome({ name }: { name: string }) {
+  return <h1>Hello, {name}</h1>;
+}
+
 function App() {
-  const [count, setCount] = useState(0);
+
+
+
+  const [name, setName] = useState("");
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 6)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      s
+      <input
+        placeholder="Enter your name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+
+      <Welcome name={name} />
+    </div>
   );
 }
 
 export default App;
-
