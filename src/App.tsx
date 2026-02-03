e2faa4ca032bcf8f36c1bf2a4bc25da23240e9fe
back-02-04 <back-02-04@Erxess-Mac-mini.local>
2026-02-03 20:33:20 +0800

    aasas

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,14 @@
-import { useEffect, useState } from "react";
 import "./App.css";
+import { MovieMain } from "./modules/components/MainMovie";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-interface IMovie {
-  title: string;
-}
+const queryClient = new QueryClient();
 
 function App() {
-  const [movies, setMovies] = useState<IMovie[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    fetch("http://localhost:3000/movies/movies")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setLoading(false);
-        setMovies(data);
-      });
-  }, []);
-
-  if (loading) {
-    return <h1>Unshij bn</h1>;
-  }
-
   return (
-    <>
-      {movies.map((movie) => {
-        return <h1>{movie.title}</h1>;
-      })}
-    </>
+    <QueryClientProvider client={queryClient}>
+      <MovieMain />
+    </QueryClientProvider>
   );
 }
 
